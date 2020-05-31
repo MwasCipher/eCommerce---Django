03b16fee4b39ad166331b70285c9f334f73dbd12
MwasCipher <mwangicollinswaweru@gmail.com>
2020-05-31 14:50:17 +0300

    Stripe Card Elements

--- billing/views.py
+++ billing/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+import stripe
 
 # Create your views here.
+STRIPE_PUBLIC_KEY = ''
+stripe.api_key = 'sk_test_EkmThKAelBXpI5emhMFE2fns00YtrrQBaZ'
+
+
+def stripe_payment_view(request):
+    context = {
+        'public_key': STRIPE_PUBLIC_KEY,
+
+    }
+    if request.method == 'POST':
+        print(request.POST)
+    return render(request, 'stripe_payment.html', context)
