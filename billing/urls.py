0c78a8d0ddf99fcd9b725ed833f3474866bac6b2
MwasCipher <mwangicollinswaweru@gmail.com>
2020-05-31 15:00:47 +0300

    Stripe Card Elements

--- billing/urls.py
+++ billing/urls.py
@@ -4,5 +4,5 @@ from django.contrib.auth.urls import urlpatterns
 from billing.views import stripe_payment_view
 
 urlpatterns = [
-    url('/stripe/', stripe_payment_view, name='stripe_payment')
+    url('stripe/', stripe_payment_view, name='stripe_payment')
 ]
