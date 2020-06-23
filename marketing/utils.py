02733147aecd2a3563552b060e1362b797f2e2b2
MwasCipher <mwangicollinswaweru@gmail.com>
2020-06-23 13:06:09 +0300

    Mailchimp

--- marketing/utils.py
+++ marketing/utils.py
@@ -1,6 +1,19 @@
 from django.conf import settings
+import requests
+
+MAILCHIMP_API_KEY = getattr(settings, 'MAILCHIMP_API_KEY', None)
+MAILCHIMP_DATA_CENTER = getattr(settings, 'MAILCHIMP_DATA_CENTER', None)
+MAILCHIMP_EMAIL_LIST_ID = getattr(settings, 'MAILCHIMP_EMAIL_LIST_ID', None)
 
 
 class MailChimp(object):
     def __init__(self):
         super(MailChimp, self).__init__()
+        self.key = MAILCHIMP_API_KEY
+        self.api_url = "https://{dc}.api.mailchimp.com/3.0".format(dc=MAILCHIMP_DATA_CENTER)
+        self.list_id = MAILCHIMP_EMAIL_LIST_ID
+
+    def check_subscription_status(self, email):
+        endpoint = self.api_url
+        request_object = requests.get(endpoint, auth=('', self.key))
+        return request_object.json()
