cdbe2785d8ada6f391ac58962bfb21324ff27e41
MwasCipher <mwangicollinswaweru@gmail.com>
2020-06-24 18:32:42 +0300

    Deployment, .gitignore File

--- ecom/settings/local.py
+++ ecom/settings/local.py
@@ -155,3 +155,13 @@ STATICFILES_DIRS = [
                                                   # Found in My Project's Directory
 ]
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'ecom/media')
+
+CORS_REPLACE_HTTPS_REFERER = False
+HOST_SCHEME = 'http://'
+SECURE_PROXY_SSL_HEADER = None
+SECURE_SSL_REDIRECT = False
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
+SECURE_HSTS_INCLUDE_SUBDOMAINS = False
+SECURE_HSTS_SECONDS = None
+SECURE_FRAME_DENY = False
