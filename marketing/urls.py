ec3f547bc4e4d0dc145964272b322798a6c26842
MwasCipher <mwangicollinswaweru@gmail.com>
2020-06-24 12:12:49 +0300

    Mailchimp, Update Subscription, CSRF Exempt Mixin

--- marketing/urls.py
+++ marketing/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from marketing.views import MarketingPreferenceUpdateView
+from marketing.views import MarketingPreferenceUpdateView, MailChimpWebHookView
 
 urlpatterns = [
     url('update_pref/', MarketingPreferenceUpdateView.as_view(), name='marketing_email'),
+    url('webhook/', MailChimpWebHookView.as_view(), name='web_hook'),
 ]
--- marketing/views.py
+++ marketing/views.py
@@ -71,31 +71,31 @@ class MailChimpWebHookView(CsrfExemptMixin, View):
         return HttpResponse('Thank You', status=200)
 
 
-def mail_chimp_webhook_view(request):
-    data = request.POST
-    list_id = data.get('data[list_id]')
-    if str(list_id) == str(MAILCHIMP_EMAIL_LIST_ID):
-        email = data.get('data[email]')
-        web_hook_type = data.get('type')
-        response_status, response = MailChimp.change_subscription_status(email)
-        subscription_status = response_status['status']
-
-        is_subscribed = None
-        is_mail_chimp_subscribed = None
-
-        if subscription_status == 'subscribed':
-            is_subscribed = True
-            is_mail_chimp_subscribed = True
-
-        elif subscription_status == 'unsubscribed':
-            is_subscribed = False
-            is_mail_chimp_subscribed = False
-
-        if is_subscribed is not None and is_mail_chimp_subscribed is not None:
-            qs = MarketingPreference.objects.filter(user__email__iexact=email)
-            if qs.exists():
-                qs.update(subscribed=is_subscribed,
-                          mail_chimp_subscribed=is_mail_chimp_subscribed,
-                          is_mail_chimp_message=str(data))
-
-    return HttpResponse('Thank You', status=200)
+# def mail_chimp_webhook_view(request):
+#     data = request.POST
+#     list_id = data.get('data[list_id]')
+#     if str(list_id) == str(MAILCHIMP_EMAIL_LIST_ID):
+#         email = data.get('data[email]')
+#         web_hook_type = data.get('type')
+#         response_status, response = MailChimp.change_subscription_status(email)
+#         subscription_status = response_status['status']
+#
+#         is_subscribed = None
+#         is_mail_chimp_subscribed = None
+#
+#         if subscription_status == 'subscribed':
+#             is_subscribed = True
+#             is_mail_chimp_subscribed = True
+#
+#         elif subscription_status == 'unsubscribed':
+#             is_subscribed = False
+#             is_mail_chimp_subscribed = False
+#
+#         if is_subscribed is not None and is_mail_chimp_subscribed is not None:
+#             qs = MarketingPreference.objects.filter(user__email__iexact=email)
+#             if qs.exists():
+#                 qs.update(subscribed=is_subscribed,
+#                           mail_chimp_subscribed=is_mail_chimp_subscribed,
+#                           is_mail_chimp_message=str(data))
+#
+#     return HttpResponse('Thank You', status=200)
