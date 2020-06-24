9d044228ee2b3a25240695b1eae7e6bc45e63d70
MwasCipher <mwangicollinswaweru@gmail.com>
2020-06-24 11:05:33 +0300

    Mailchimp, Update Subscription

--- marketing/views.py
+++ marketing/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import UpdateView
 
@@ -12,13 +13,21 @@ class MarketingPreferenceUpdateView(UpdateView):
     template_name = 'marketing_form.html'
     success_url = 'update_pref'
 
+    def dispatch(self, request, *args, **kwargs):
+        user = self.request.user
+        if not user.is_authenticated():
+            return HttpResponse('Not Allowed', status=400)
+
+        return super(MarketingPreferenceUpdateView, self).dispatch(request, *args, **kwargs)
+
     def get_context_data(self, *args, **kwargs):
         context = super(MarketingPreferenceUpdateView, self).get_context_data(*args, **kwargs)
         context['title'] = 'Update Email Preferences'
 
     def get_object(self):
         user = self.request.user
-        marketing_preference_object, create_marketing_preference_object = MarketingPreference.objects.get_or_create(user=user)
+        marketing_preference_object, create_marketing_preference_object = MarketingPreference.objects.get_or_create(
+                                                                                                           user=user)
         return marketing_preference_object
 
 
