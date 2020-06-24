c1a0eb97b4854277f46ba3d3322a0f10d97a2f2e
MwasCipher <mwangicollinswaweru@gmail.com>
2020-06-24 11:57:12 +0300

    Mailchimp, Update Subscription

--- marketing/views.py
+++ marketing/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views.generic import UpdateView
@@ -5,6 +6,10 @@ from django.contrib.messages.views import SuccessMessageMixin
 
 from .forms import MarketingPrefenceForm
 from .models import MarketingPreference
+from .utils import MailChimp
+
+MAILCHIMP_EMAIL_LIST_ID = getattr(settings, 'MAILCHIMP_EMAIL_LIST_ID', None)
+
 
 # Create your views here.
 
@@ -29,7 +34,33 @@ class MarketingPreferenceUpdateView(UpdateView, SuccessMessageMixin):
     def get_object(self):
         user = self.request.user
         marketing_preference_object, create_marketing_preference_object = MarketingPreference.objects.get_or_create(
-                                                                                                           user=user)
+            user=user)
         return marketing_preference_object
 
 
+def mail_chimp_webhook_view(request):
+    data = request.POST
+    list_id = data.get('data[list_id]')
+    if str(list_id) == str(MAILCHIMP_EMAIL_LIST_ID):
+        email = data.get('data[email]')
+        web_hook_type = data.get('type')
+        response_status, response = MailChimp.change_subscription_status(email)
+        subscription_status = response_status['status']
+
+        is_subscribed = None
+        is_mail_chimp_subscribed = None
+
+        if subscription_status == 'subscribed':
+            is_subscribed = True
+            is_mail_chimp_subscribed = True
+
+        elif subscription_status == 'unsubscribed':
+            is_subscribed = False
+            is_mail_chimp_subscribed = False
+
+        if is_subscribed and is_mail_chimp_subscribed:
+            qs = MarketingPreference.objects.filter(user__email__iexact=email)
+            if qs.exists():
+                qs.update(subscribed=is_subscribed, mail_chimp_subscribed=is_mail_chimp_subscribed)
+
+    return
