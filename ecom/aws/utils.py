07b25172ef88d30c414140c2c398f57e686edda5
MwasCipher <mwangicollinswaweru@gmail.com>
2020-06-29 16:16:01 +0300

    AWS Configuration For Static Files

---
+++ ecom/aws/utils.py
@@ -0,0 +1,2 @@
+StaticRootS3BotoStorage = lambda: S3BotoStorage(location='static')
+MediaRootS3BotoStorage = lambda: S3BotoStorage(location='media')
--- ecom/settings/local.py
+++ ecom/settings/local.py
@@ -156,6 +156,8 @@ STATICFILES_DIRS = [
 ]
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'ecom/media')
 
+from ecom.aws.conf import *
+
 CORS_REPLACE_HTTPS_REFERER = False
 HOST_SCHEME = 'http://'
 SECURE_PROXY_SSL_HEADER = None
